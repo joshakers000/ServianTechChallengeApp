95cae61bdc027df4d898bd7bcd52e5b1f7cbb410
joshakers000 <80078796+joshakers000@users.noreply.github.com>
2021-03-30 02:52:10 +1100

    Update move.py
    Corrected the long string.

--- move.py
+++ move.py
@@ -7,16 +7,19 @@ import sys
 try:
 	EC2 = sys.argv[1].strip()
 except:
-	print()
+	print("Please include your EC2-Endpoint.")
+	sys.exit(1)
 
 try:
 	os.system("sudo scp -i WebSeverKeyPair.pem -r /dump ec2-user@" + EC2 + ":/home/ec2-user")
 	os.system("sudo scp -i WebSeverKeyPair.pem  deploy.sh ec2-user@" +  EC2 + ":/home/ec2-user")
 	# Recursive scp has strange functionality when it comes to updated files.
-	os.system("sudo scp -i WebSeverKeyPair.pem dump/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/"
-		  "TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/conf.toml  ec2-user@" + EC2 + "
-		  ":/home/ec2-user/dump/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/TechChallengeApp-
-		  "70138b5a1badeada55276a8275c6f2b6de47d771/")
+	os.system("sudo scp -i WebSeverKeyPair.pem dump/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/"\
+		  + "TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/conf.toml  ec2-user@"\
+                  + EC2\
+		  + ":/home/ec2-user/dump/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/TechChallengeApp-"\
+		  + "70138b5a1badeada55276a8275c6f2b6de47d771/")
 	os.system("ssh -i WebSeverKeyPair.pem ec2-user@" + EC2)
 except Exception as e:
 	print(e)
+	sys.exit(1)
