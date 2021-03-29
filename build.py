3717d85787f9767a6b66434aa7404d46755c8f3a
joshakers000 <80078796+joshakers000@users.noreply.github.com>
2021-03-30 02:53:36 +1100

    Update build.py

--- build.py
+++ build.py
@@ -3,12 +3,16 @@
 # Description: This starts the build process by creating a key pair and creating the CloudFormation stack.
 
 import os
+import sys
+
 # 1. Make Keypair and change perms
 try:
 	os.system("sudo aws ec2 create-key-pair --key-name WebServerKP198 --query 'KeyMaterial' --output text > WebSeverKeyPair.pem")
 	os.system("sudo chmod 400 WebSeverKeyPair.pem")
 except Exception as e:
 	print(e)
+	sys.exit(1)
+	
 # 2. Run CloudFormation script
 os.system("sudo aws cloudformation create-stack --stack-name WebServerDeployment --template-body file://techchallenge_CF.yaml --parameters file://stackparams.json --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM")
 print("Please obtain the dns endpoint for the postgres instance and the EC2 instance.  Stack completion will take approximately eight minutes.")
