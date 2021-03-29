348173fcc9727f8ef31ac7dee0b0b94f9a57a225
joshakers000 <80078796+joshakers000@users.noreply.github.com>
2021-03-29 05:34:07 -0700

    Add files via upload
    Added the deploy script for deploying the web application once ssh'd in.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo yum -y install go
+cd ../
+mkdir go
+mkdir go/bin
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+cd dump/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771/TechChallengeApp-70138b5a1badeada55276a8275c6f2b6de47d771
+sudo bash build.sh
+cd dist
+sudo ./TechChallengeApp updatedb -s
+sudo ./TechChallengeApp serve
